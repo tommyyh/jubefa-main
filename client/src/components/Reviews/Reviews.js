035f6ade1a5b9de94da0c1b70f54a9c6a453c60e
Tommy Hoang <tommyyhoangg@gmail.com>
2023-10-31 23:05:52 +0100

    Added why us for desktop

--- client/src/components/Reviews/Reviews.js
+++ client/src/components/Reviews/Reviews.js
@@ -7,16 +7,20 @@ const Reviews = ({ lang, langCode }) => {
   const l = lang.reviews;
 
   return (
-    <section className={css['reviews']}>
-      <h4>{l.sub}</h4>
-      <h2>{l.title}</h2>
+    <div className={css['cont']}>
+      <section className={css['reviews']}>
+        <div className={css['top']}>
+          <h4>{l.sub}</h4>
+          <h2>{l.title}</h2>
+        </div>
 
-      <div className={css['content']}>
-        {Object.keys(l.list).map((item, i) => (
-          <Review lang={lang} item={item} langCode={langCode} key={i} />
-        ))}
-      </div>
-    </section>
+        <div className={css['content']}>
+          {Object.keys(l.list).map((item, i) => (
+            <Review lang={lang} item={item} langCode={langCode} key={i} />
+          ))}
+        </div>
+      </section>
+    </div>
   );
 };
 
@@ -25,23 +29,25 @@ const Review = ({ lang, item }) => {
 
   return (
     <div className={css['review']}>
-      <img className={css['profile']} src={profileImgPng} alt='profile' />
+      <div>
+        <img className={css['profile']} src={profileImgPng} alt='profile' />
 
-      <a
-        href='https://www.google.com/search?q=jubefa+gmbh&oq=jubefa+gmbh+&gs_lcrp=EgZjaHJvbWUyBggAEEUYOTIGCAEQRRg8MgYIAhBFGDwyBggDEEUYPNIBCDI1MzNqMGo3qAIAsAIA&sourceid=chrome&ie=UTF-8#lrd=0x4709cf176bc98ac3:0x586731f7e651a33e,1,,,,'
-        target='_blank'
-        className={css['author']}
-      >
-        <h5>{l.author}</h5>
-        <img
-          className={css['new-window']}
-          src={newWindow2Svg}
-          alt='new window'
-        />
-      </a>
+        <a
+          href='https://www.google.com/search?q=jubefa+gmbh&oq=jubefa+gmbh+&gs_lcrp=EgZjaHJvbWUyBggAEEUYOTIGCAEQRRg8MgYIAhBFGDwyBggDEEUYPNIBCDI1MzNqMGo3qAIAsAIA&sourceid=chrome&ie=UTF-8#lrd=0x4709cf176bc98ac3:0x586731f7e651a33e,1,,,,'
+          target='_blank'
+          className={css['author']}
+        >
+          <h5>{l.author}</h5>
+          <img
+            className={css['new-window']}
+            src={newWindow2Svg}
+            alt='new window'
+          />
+        </a>
 
-      <div className={css['text']}>
-        <p>{l.text}</p>
+        <div className={css['text']}>
+          <p>{l.text}</p>
+        </div>
       </div>
 
       <h6>{l.rating}/5</h6>
